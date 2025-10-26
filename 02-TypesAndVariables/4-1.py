cb7c81a338a8696cc56d3ee38aa7a54e52ce9b8a
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-10-26 17:53:37 +0100

    I finished more tasks

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,11 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age1 = 18
+height = 185
+age2 = 24
+
+print(f"My name is {name}.")
+print(f"I am {age1} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age2} years old.")

9d2c5baebc0f38f93258a86a682f6afeae90c835
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-11-02 16:48:40 +0100

    I fixed something

--- 02-TypesAndVariables/5-1.py
+++ 02-TypesAndVariables/5-1.py
@@ -5,4 +5,4 @@ first_name = input('Enter your first name: ')
 last_name = input('Enter your last name: ')
 full_name = first_name + ' ' + last_name
 print(f'Your first name is {first_name} and your last name is {last_name}')
-print(f'And your full name is {" ".join((first_name, last_name))}')
+print(f'And your full name is {full_name}')

e36613fe28bba515ca9dff1a2e4239df35d73a75
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-11-07 12:48:54 +0100

    I did some more tasks

---
+++ 03-ControlStructures/2-6.py
@@ -0,0 +1,10 @@
+number = int(input('Enter number: '))
+
+if number > 0:
+    print(f'Number {number} is positive')
+
+elif number <0:
+    print(f'Number {number} is negative')
+
+else:
+    print('Number is 0')

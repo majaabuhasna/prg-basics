06a8324d9edfa3f60fea107956ae7bbdea42d415
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-11-11 19:52:36 +0100

    I did one more task

---
+++ 03-ControlStructures/6-24.py
@@ -0,0 +1,9 @@
+for x in range(0,5):
+    for y in range(0, x+1):
+        print('*', end='')
+    print()
+
+for x in range(5,9):
+    for y in range(x-1,8):
+        print('*', end='')
+    print()

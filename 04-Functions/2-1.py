af54ff5c718f0751f93a3a305495a9730f72ba46
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-11-14 12:14:41 +0100

    I did one task

---
+++ 04-Functions/2-1.py
@@ -0,0 +1,29 @@
+###
+# Program for testing built-in functions
+#
+max_number = max(7,5,6,3,8,2)
+print('Max number of 7,5,6,3,8,2 is', max_number)
+
+min_number = min(4,7,2,3,9,8)
+print('Min number of 4,7,2,3,9,8 is', min_number)
+
+str_length = len("computer science")
+print('Number of characters in "computer science" is', str_length)
+
+letter = input("Enter letter: ")
+print('Letter read from the keyboard is', letter)
+
+number_representing_string = int("20303")
+print('Number representing the string "20303" is', number_representing_string)
+
+binary = bin(304)
+print('Binary string representing decimal number 304 is', binary)
+
+hexadecimal = hex(304)
+print('Hexadecimal string representing decimal number 304', hexadecimal)
+
+integer_unicode = ord('€')
+print('Integer representing the Unicode code of the € sign is', integer_unicode)
+
+absolute_value = abs(-17)
+print('Absolute value of -17 is', absolute_value)

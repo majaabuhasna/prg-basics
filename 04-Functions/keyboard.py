05219ae1069ec5b4972f7a6b032db5506e501a38
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-11-16 15:58:46 +0100

    I created keyboard.py

---
+++ 04-Functions/keyboard.py
@@ -0,0 +1,18 @@
+###
+# Functions to read any data type from the keyboard
+#
+def input_string(message):
+    str = str(input(message))
+    return str
+
+def input_integer(message):
+    int = int(input(message))
+    return int
+
+def input_real(message):
+    real = real(input(message))
+    return real
+
+def input_boolean(message):
+    bool = bool(input(message))
+    return bool

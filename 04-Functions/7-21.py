3b70bb0d6439b9d2deb4c22a4035d7f50fd940e4
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-11-20 00:42:35 +0100

    I did one more

---
+++ 04-Functions/7-21.py
@@ -0,0 +1,9 @@
+def f(number1,number2,number3):
+    max_number = max(number1,number2,number3)
+    min_number = min(number1,number2,number3)
+
+    difference = max_number - min_number
+    return print(difference)
+
+f(7,4,9)
+f(2,12,8)

a1d42efea8f0362218ae714d399d3e41058a095d
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-11-20 00:38:47 +0100

    I did one more

---
+++ 04-Functions/7-20.py
@@ -0,0 +1,23 @@
+def f(n):
+    counter = 0
+    candidate = 2
+
+    while counter < n:
+        is_prime = True
+        sqrt = candidate ** 0.5
+
+        for i in range(2, sqrt+1):
+            if candidate % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            counter += 1
+            if counter == n:
+                return candidate
+            
+        candidate += 1
+
+print(1)
+print(5)
+        

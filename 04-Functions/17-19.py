fef47b8f898bf67e6c72522beb0d1d0aeaba819b
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-11-20 00:03:53 +0100

    I did one more task

---
+++ 04-Functions/17-19.py
@@ -0,0 +1,11 @@
+def f(number):
+    number = str(number)
+    sum = 0
+    for digit in number:
+        if number.count(digit) > 1:
+            sum += int(digit)
+    return print(sum)
+
+f(1027)
+f(230335)
+f(513553007)

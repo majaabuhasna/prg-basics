465f489b3c93e6b6ce1f169c3f3e20170abe7d0f
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-11-20 19:46:16 +0100

    I did one more task

---
+++ 04-Functions/7-26.py
@@ -0,0 +1,14 @@
+def f(text):
+    separated_text = ''
+
+    if text == "":
+        return '""'
+
+    for char in text[:-1:]:
+        separated_text += char + '-'
+    for char in text[-1]:
+        separated_text += char
+
+    return separated_text
+
+print(f("Univesity"), f("UE"), f("x"), f(""), sep='\n')

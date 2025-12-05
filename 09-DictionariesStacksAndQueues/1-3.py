028e04313941125dc3c9296e470b4e8e333de1f4
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-12-05 11:57:00 +0100

    I did one task

---
+++ 09-DictionariesStacksAndQueues/1-3.py
@@ -0,0 +1,10 @@
+mobile = {
+"OS":"iOS",
+"Colour": "White",
+"Storage": "128 GB",
+"Model": "12",
+"Face ID": "Yes",
+"Price": "800 USD"
+}
+for key,value in mobile.items():
+   print(f"{key} : {value}")

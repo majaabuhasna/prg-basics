de296a9e90d8bf5d15145973bfbbacec17a79206
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-12-05 12:54:12 +0100

    I did one more

---
+++ 08-FileHandling/3-4.py
@@ -0,0 +1,28 @@
+###
+# Calculates the total value of money spent
+#
+import re # module for regular expressions
+
+# file name with shopping report
+email_file = 'report.txt'
+
+# read the content of email
+with open(email_file, 'r', encoding='utf-8') as email_content:
+    email = email_content.read()
+
+# regular expression pattern
+# for amounts
+pattern = r'€\d+'
+
+# extract numbers from email
+# tip: findall() method returns an array
+amounts = re.findall(pattern, email)
+
+# calculate the total purchases
+total = 0
+for amount in amounts:
+    amount = int(amount[1:])
+    total += amount
+    
+# print result
+print(total)

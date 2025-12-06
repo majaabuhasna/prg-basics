0458ae6161666197ce61b731111f28790416e6fd
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-12-06 21:38:44 +0100

    I did one more

---
+++ 07-Arrays/3-9.py
@@ -0,0 +1,13 @@
+def compare(array1, array2):
+    if len(array1) != len(array2):
+        return False
+    
+    elif array1!= array2:
+            return False
+        
+    return True
+
+print(compare(["water","book","sky"],["water","book","sky"]))
+print(compare([True,False],[True,False,True]))
+print(compare([5,3,1],[5,3,1]))
+print(compare([3,2,1],[3,2]))

71b9e2daa6e71c246269412e1a88e45601ec2798
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-12-06 20:44:16 +0100

    I did one more task

---
+++ 07-Arrays/3-2.py
@@ -0,0 +1,19 @@
+arr = [15, 8, 31, 47, 2, 19]
+arr_reversed = []
+
+i = 0
+j = -1
+while i < len(arr):
+    x = arr[j]
+    i += 1
+    j += -1
+
+    arr_reversed.append(x)
+
+print(arr_reversed)
+
+arr_reversed2 = arr[::-1]
+print(arr_reversed2)
+
+arr_reversed3 = list(reversed(arr))
+print(arr_reversed3)

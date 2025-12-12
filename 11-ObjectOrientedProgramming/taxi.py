0fc82ac0c37275ce34c0ef8357c37f43950166bf
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-12-12 10:50:04 +0100

    I did one more

--- 11-ObjectOrientedProgramming/taxi.py
+++ 11-ObjectOrientedProgramming/taxi.py
@@ -8,12 +8,23 @@ class TaxiRide:
         self.distance = distance
         self.fare = self.distance * self.rate_per_km
 
+    def print_receipt(self):
+        # Prints the details of the ride
+        print("Taxi ride receipt:")
+        print(f"Distance: {self.distance} km")
+        print(f"Rate per km: €{self.rate_per_km}")
+        print(f"Fare: €{self.fare:.2f}\n")
 
 def main():
     # your program
-    ...
-    ...
-    ...
+    ride1 = TaxiRide(rate_per_km=2.50)
+    ride2 = TaxiRide(rate_per_km=3.00)
+
+    ride1.calculate_fare(15)
+    ride2.calculate_fare(10)
+
+    ride1.print_receipt()
+    ride2.print_receipt()
 
 if __name__ == "__main__":
     main()

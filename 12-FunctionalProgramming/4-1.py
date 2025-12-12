a69093a4e66803e1bc0ac8b8e1dd6876a979a114
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-12-12 12:44:38 +0100

    I did

---
+++ 12-FunctionalProgramming/4-1.py
@@ -0,0 +1,4 @@
+employees = ["SMITH Lucy","JONES Janet","LEE Jerry",
+               "JACKSON Peter","JOHNSON Rick",
+               "LEWIS Terry","CLARKE Robin"]
+emp_J = print(list(filter(lambda e:e[0]=="J", employees)))

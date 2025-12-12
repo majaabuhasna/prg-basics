729d9dd717b05c123f382b85601da11ca8ea9092
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-12-12 11:07:42 +0100

    I did one more task

---
+++ 11-ObjectOrientedProgramming/sroo-2.py
@@ -0,0 +1,17 @@
+# class definition
+class Song:
+   def __init__(self,performer,title,album,year):
+      self.performer = performer
+      self.title = title
+      self.album = album
+      self.year = year
+   def __str__(self):
+      return f"Performer: {self.performer}\n Title: {self.title}\n Album: {self.album}\n Year: {self.year}\n"
+
+# object creation
+song1 = Song("Ed Sheeran","Hearts Don't Break Around Here","Divide",2017)
+song2 = Song("Queen","Bohemian Rhapsody","A Night at the Opera",1975)
+
+## object usage
+print(song1)
+print(song2)

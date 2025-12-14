a137a3886899a98d4ede9e92fab067b04a33f126
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-12-14 18:54:18 +0100

    weeee

---
+++ 08-FileHandling/emails.py
@@ -0,0 +1,34 @@
+import re
+
+def email_sender(text):
+    pattern = 'From:.*<([^>]+)>'
+    match = re.search(pattern,text)
+
+    if match:
+        return match.group(1)
+    return None
+
+def email_recipient(text):
+    pattern = 'To:.*<([^>]+)>'
+    match = re.search(pattern,text)
+
+    if match:
+        return match.group(1)
+    return None
+
+def email_subject(text):
+    pattern = 'Subject: (.*)'
+    match = re.search(pattern,text)
+
+    if match:
+        return match.group(1)
+    return None
+
+def email_body(text):
+    pattern = '\n\n(.*)'
+
+    match = re.search(pattern,text)
+
+    if match:
+        return match.group(1).strip()
+    return None

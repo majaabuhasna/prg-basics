ef40596e8e12edb98aa8182472a4761c634a4111
Maja Abuhasna <242409@student.uek.krakow.pl>
2025-12-15 19:02:08 +0100

    I did one more

--- 09-DictionariesStacksAndQueues/5-13.py
+++ 09-DictionariesStacksAndQueues/5-13.py
@@ -1,11 +1,45 @@
-import queue
-
-stack = queue.LifoQueue()
+stack = []
 
 expression = {}
 
 while True:
-    value = input('Enter any number, an operator (+ - * / ) or the equal sign (=): ')
+    value = input('Enter any number, an operator (+ - * / ) or the equal sign (=): ').strip()
 
-    if value == float(value):
-        
+    if value == '=':
+        if len(stack)>0:
+            print(stack.pop())
+        else:
+            print('No result')
+        break
+
+    elif value == '+' or value == '-' or value == '*' or value == '/':
+        if len(stack)<2:
+            print('Not enough numbers in stack to continue')
+            continue
+
+        item1 = stack.pop()
+        item2 = stack.pop()
+
+        result = 0
+        if value == '+':
+            result = item2 + item1
+        elif value == '-':
+            result = item2 - item1
+        elif value == '*':
+            result = item2 * item1
+        elif value == '/':
+            if item1 == 0:
+                print('Cannot divide by 0')
+                stack.append(item2)
+                stack.append(item1)
+                continue
+            result = item2 / item1
+
+        stack.append(result)
+    
+    else:
+        try:
+            number = float(value)
+            stack.append(number)
+        except ValueError:
+            print('Unknown command')

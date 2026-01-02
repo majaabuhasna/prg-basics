0b5220046765ebced5016f2bb60f7230f19db356
Maja Abuhasna <242409@student.uek.krakow.pl>
2026-01-02 22:03:08 +0100

    I did one more

--- 11-ObjectOrientedProgramming/tv.py
+++ 11-ObjectOrientedProgramming/tv.py
@@ -5,6 +5,19 @@ class TV:
       self.is_on = False
       self.channel_no = 1
       self.channels = []
+      self.volume = 0
+
+   def volume_increase(self):
+      if self.volume < 10:
+         self.volume += 1
+      else:
+         print('Error! Maximum volume reached.')
+
+   def volume_decrease(self):
+      if self.volume > 0:
+         self.volume -= 1
+      else:
+         print('Error! Minimum volume reached.')
 
    def turn_off(self):
       self.is_on = False
@@ -21,6 +34,7 @@ class TV:
             print(f'TV is on, channel {self.channel_no}')
       else:
          print('TV is off')
+      print(f"Volume is on level {self.volume}\n")
 
    def set_channels(self, channels_list):
       self.channels = channels_list
--- 11-ObjectOrientedProgramming/tv_show.py
+++ 11-ObjectOrientedProgramming/tv_show.py
@@ -8,15 +8,27 @@ def main():
 
    # object usage
     my_tv.show_status()
-
     my_tv.turn_on()
-    my_tv.show_status()
+    my_tv.volume_increase()
+    my_tv.volume_increase()
 
     channels_data = ["TVP1", "TVP2", "Polsat", "TVN", "Filmbox", "Discovery","Animal Planet"]
     my_tv.set_channels(channels_data)
-    my_tv.show_channels()
+    my_tv.set_channel(2)
+    my_tv.show_status()
+    my_tv.volume_increase()
+    my_tv.volume_increase()
+    my_tv.volume_increase()
 
-    my_tv.turn_off()
+    my_tv.set_channel(5)
+    my_tv.show_status()
+
+    my_tv.set_channel(7)
+    my_tv.show_status()
+    my_tv.volume_decrease()
+    my_tv.volume_decrease()
+
+    my_tv.set_channel(8)
     my_tv.show_status()
 
 if __name__ == "__main__":

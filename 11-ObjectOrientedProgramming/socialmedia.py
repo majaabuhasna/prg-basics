146586b355334e0a747b5e4abde1ee4981700df0
Maja Abuhasna <242409@student.uek.krakow.pl>
2026-01-02 21:17:25 +0100

    I did one task

--- 11-ObjectOrientedProgramming/socialmedia.py
+++ 11-ObjectOrientedProgramming/socialmedia.py
@@ -7,3 +7,18 @@ class SocialMediaProfile:
         self.posts.append(content)
         print(f"{self.username} added a new post: {content}")
 
+    def display_timeline(self):
+        print(f'Username: {self.username}')
+        index = 1
+        for post in self.posts:
+            print(f'{index}. {post}')
+            index += 1
+
+if __name__ == '__main__':
+    user = SocialMediaProfile('johndoe')
+    user.add_post("Hello, world!")
+    user.add_post("Had a great day at the park!")
+    user.add_post("What's up, Natalie? How are you?")
+
+    print('\n--- TIMELINE ---')
+    user.display_timeline()

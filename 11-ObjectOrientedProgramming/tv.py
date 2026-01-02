ecaa4163612e918d1748747785814dd9546a1fc2
Maja Abuhasna <242409@student.uek.krakow.pl>
2026-01-02 21:54:03 +0100

    I did one more

--- 11-ObjectOrientedProgramming/tv.py
+++ 11-ObjectOrientedProgramming/tv.py
@@ -4,16 +4,33 @@ class TV:
    def __init__(self):
       self.is_on = False
       self.channel_no = 1
+      self.channels = []
+
    def turn_off(self):
       self.is_on = False
+
    def turn_on(self):
       self.is_on = True
+
    def show_status(self):
-      if self.is_on == True:
-         print(f'TV is on, channel {self.channel_no}')
+      if self.is_on:
+         if self.channel_no <= len(self.channels):
+            channel_name = self.channels[self.channel_no - 1]
+            print(f'TV is on, channel {self.channel_no} ({channel_name})')
+         else:
+            print(f'TV is on, channel {self.channel_no}')
       else:
          print('TV is off')
+
+   def set_channels(self, channels_list):
+      self.channels = channels_list
+
+   def show_channels(self):
+      print("Channels list:")
+      index = 1
+      for channel in self.channels:
+         print(f"{index}. {channel}")
+
    def set_channel(self,new_channel_no):
       if self.is_on:
-            self.channel_no = new_channel_no
-      
+         self.channel_no = new_channel_no
--- 11-ObjectOrientedProgramming/tv_show.py
+++ 11-ObjectOrientedProgramming/tv_show.py
@@ -12,8 +12,9 @@ def main():
     my_tv.turn_on()
     my_tv.show_status()
 
-    my_tv.set_channel(5)
-    my_tv.show_status()
+    channels_data = ["TVP1", "TVP2", "Polsat", "TVN", "Filmbox", "Discovery","Animal Planet"]
+    my_tv.set_channels(channels_data)
+    my_tv.show_channels()
 
     my_tv.turn_off()
     my_tv.show_status()

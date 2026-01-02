6a595209f90c77ca983b59d5041d55aabdacfe19
Maja Abuhasna <242409@student.uek.krakow.pl>
2026-01-02 21:37:25 +0100

    A small update

--- 11-ObjectOrientedProgramming/sroo-2.py
+++ 11-ObjectOrientedProgramming/sroo-2.py
@@ -6,7 +6,7 @@ class Song:
       self.album = album
       self.year = year
    def __str__(self):
-      return f"Performer: {self.performer}\n Title: {self.title}\n Album: {self.album}\n Year: {self.year}\n"
+      return f"Performer: {self.performer}\n Title: {self.title}\n Album: {self.album}\n Year: {self.year}"
 
 # object creation
 song1 = Song("Ed Sheeran","Hearts Don't Break Around Here","Divide",2017)
@@ -14,4 +14,4 @@ song2 = Song("Queen","Bohemian Rhapsody","A Night at the Opera",1975)
 
 ## object usage
 print(song1)
-print(song2)
+print(f"\n{song2}")

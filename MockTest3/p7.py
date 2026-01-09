684c8618e601b42cef392d5bac0378c509ca8cfe
Maja Abuhasna <242409@student.uek.krakow.pl>
2026-01-09 12:18:09 +0100

    I did one more

---
+++ MockTest3/p7.py
@@ -0,0 +1,35 @@
+class C:
+    def __init__(self,value):
+        self.value = value
+
+    def  m1(self):
+        return self.value
+    
+    def m2(self):
+        self.value += 1
+
+    def m3(self):
+        self.value -= 1
+
+    def m4(self,n):
+        self.value += n
+
+    def __str__(self):
+        return str(self.value)
+    
+def main():
+    c=C(5)
+    r1 = c.m1()
+    c.m2()
+    r2 = c.m1()
+    c.m4(-8)
+    r3 = c.m1()
+    c.m3()
+    r4 = c.m1()
+    c.m4(10)
+    r5 = c.m1()
+    c.__str__()
+
+    return r1,r2,r3,r4,r5
+
+print(main())

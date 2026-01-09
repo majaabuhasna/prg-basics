b83f754be25955d8750dd03209784f19ff9ebabb
Maja Abuhasna <242409@student.uek.krakow.pl>
2026-01-09 11:31:08 +0100

    I did one more

--- MockTest3/p3.py
+++ MockTest3/p3.py
@@ -1,2 +1,17 @@
 def f(d):
+    number = 0
+    sum_passengers = 0
+    for key,value in d.items():
+        sum_passengers += value
+
+    average = sum_passengers / len(d)
+
+    for value in d.values():
+        if value > average:
+            number += 1
+        
+    return number
+
+print(f({"LO231":150,"BA787":120,"NZ15":30}))
+print(f({"LO231":150,"BA787":20,"NZ15":30}))
     

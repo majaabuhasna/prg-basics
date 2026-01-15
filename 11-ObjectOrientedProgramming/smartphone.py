1f6e6a76f4c10f7f19f7c2a426f7e7a00d048ac0
Maja Abuhasna <242409@student.uek.krakow.pl>
2026-01-15 19:15:38 +0100

    I did one more task

--- 11-ObjectOrientedProgramming/smartphone.py
+++ 11-ObjectOrientedProgramming/smartphone.py
@@ -1,1 +1,20 @@
-from contact import Contant
+from contact import Contact
+from contact_list import ContactList
+
+def main():
+    my_contacts = ContactList()
+
+    p1 = Contact("John Brown","brown@onet.pl","555234000")
+    p2 = Contact("Anna May","am@o2.pl","232000199")
+    p3 = Contact("George Small","smallg@google.pl","222999100")
+    p4 = Contact("Paola Big","bigpaola@poczta.pl","100200300")
+
+    my_contacts.add_contact(p1)
+    my_contacts.add_contact(p2)
+    my_contacts.add_contact(p3)
+    my_contacts.add_contact(p4)
+
+    my_contacts.display_contacts()
+
+if __name__ == '__main__':
+    main()
